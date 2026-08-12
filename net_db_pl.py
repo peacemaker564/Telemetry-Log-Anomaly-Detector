a3e939d212d5ee6c2c2e47d5dea4d1a6f898b557
peacemaker564 <jappikaur17@gmail.com>
2026-08-11 20:05:15 -0600

    Rename database and CSV file references

--- net_db_pl.py
+++ net_db_pl.py
@@ -1,8 +1,10 @@
 import sqlite3
 import csv
 
-connection = sqlite3.connect("sasktel_network.db")
+connection = sqlite3.connect("my_network.db")
 cursor = connection.cursor()
+
+cursor.execute("DROP TABLE IF EXISTS network_logs;")
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS network_logs (
             location VARCHAR(50),
@@ -13,7 +15,7 @@ cursor.execute("""
 
 """)
 
-with open("sasktel_network_logs.csv", "r") as file:
+with open("my_network_logs.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         cursor.execute("""
