eee6d8a261d47e69ec15950c3198bdf2b6a6d869
peacemaker564 <jappikaur17@gmail.com>
2026-08-11 20:06:05 -0600

    Update database connection and report file name

--- network_ai_detector.py
+++ network_ai_detector.py
@@ -3,7 +3,9 @@ import pandas as pd
 from sklearn.ensemble import IsolationForest
 import matplotlib.pyplot as plt
 
-con = sqlite3.connect("sasktel_network.db")
+file_name="my_ai_report.png"
+
+con = sqlite3.connect("my_network.db")
 query = "SELECT location, latency_ms, packet_loss_percent, status_code FROM network_logs;" #Setting table variables.
 df = pd.read_sql_query(query, con) #Uses the query(columns), and connection variable to save data into the dataframe.
 con.close() #Closes the connection.
@@ -30,6 +32,21 @@ print("\nBelow are the first few anomalies detected from data pattern:")
 anom_data = ai_anomalies[["location","latency_ms", "packet_loss_percent", "status_code"]].head()
 print(anom_data)
 
+"""
+The few lines of code below this compare the test_query script's anomaly detection, comparing the numbers provided by
+the Machine Learning Algorithm. This is fairly to compare the results of the two separate detection scripts.
+"""
+threshold_anomalies = set(df[df["latency_ms"] > 100].index)
+ai_flagged = set(ai_anomalies.index)
+
+only_threshold = threshold_anomalies - ai_flagged
+only_ai = ai_flagged - threshold_anomalies
+both = threshold_anomalies & ai_flagged
+
+print(f"\nFlagged by threshold: {len(only_threshold)}")
+print(f"Flagged by AI: {len(only_ai)}")
+print(f"\nFlagged by both: {len(both)}")
+
 
 
 # Create a scatter plot of the network metrics
@@ -44,12 +61,13 @@ anomalies = df[df["ai_prediction"] == -1]
 plt.scatter(anomalies["latency_ms"], anomalies["packet_loss_percent"], c="red", label="AI Detected Anomalies", edgecolors="black", s=100)
 
 # Add chart details
-plt.title("SaskTel Core Network Infrastructure - AI Anomaly Detection", fontsize=14)
+plt.title("My Core Network Infrastructure - AI Anomaly Detection", fontsize=14)
 plt.xlabel("Latency (ms)", fontsize=12)
 plt.ylabel("Packet Loss (%)", fontsize=12)
 plt.legend()
 plt.grid(True, linestyle="--", alpha=0.6)
 
 # Save the plot as an image file in your folder
-plt.savefig("sasktel_ai_report.png")
-print("\n[SUCCESS] Visual report saved as 'sasktel_ai_report.png'!")
+
+plt.savefig(file_name)
+print(f"\n[SUCCESS] Visual report saved as {file_name} !")
